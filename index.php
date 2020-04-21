ab5a54724925af7a8e6822d6b05e1e15e147c288
Emil Alexe <emil.alexe92@gmail.com>
2020-04-21 14:16:01 +0300

    Index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emil
+ * Date: 4/21/2020
+ * Time: 2:14 PM
+ */
+ ?>
+ <a href="theme/">Wordpress Themes</a><br>
+ <a href="plugin/">Wordpress Plugins</a>
+ <p>Copyright &copy <?php echo date('Y');?></p>
